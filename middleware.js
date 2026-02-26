10f7f180e7dcfedb2c9369f47ae15b2cee8dda24
Sneha <snehaarora68791@gmail.com>
2026-02-26 23:59:39 +0530

    remove unused detectBot middleware and related comments from Arcjet configuration

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+import arcjet, { createMiddleware, shield } from "@arcjet/next";
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -11,19 +11,10 @@ const isProtectedRoute = createRouteMatcher([
 // Create Arcjet middleware
 const aj = arcjet({
   key: process.env.ARCJET_KEY,
-  // characteristics: ["userId"], // Track based on Clerk userId
   rules: [
     // Shield protection for content and security
     shield({
       mode: "LIVE",
-    }),
-    detectBot({
-      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
-      allow: [
-        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-        "GO_HTTP", // For Inngest
-        // See the full list at https://arcjet.com/bot-list
-      ],
     }),
   ],
 });
